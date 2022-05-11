422738fd103e2102dca902b2bfa91ff69745c3b0
JustinEnlow <user@pop-os.localdomain>
2022-05-11 14:38:45 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,34 @@
 //! # Inertial Measurement Unit(IMU)
 
-use game_utils::dimension3::Dimension3;
-use game_utils::control_axis::ControlAxis;
-
 
 
 #[derive(Clone, Copy)]
 pub struct IMU<T>{
-    acceleration: ControlAxis<Dimension3<T>>,
-    velocity: ControlAxis<Dimension3<T>>
+    acceleration: T,
+    velocity: T
 }
 
 impl<T> IMU<T>
     where T: Copy
 {
-    pub fn new(zero: T) -> Self{
+    pub fn new(acceleration: T, velocity: T) -> Self{
         Self{
-            acceleration: ControlAxis::new(
-                Dimension3::new(zero, zero, zero),
-                Dimension3::new(zero, zero, zero)
-            ),
-            velocity: ControlAxis::new(
-                Dimension3::new(zero, zero, zero),
-                Dimension3::new(zero, zero, zero)
-            )
+            acceleration: acceleration,
+            velocity: velocity
         }
     }
 
-    pub fn acceleration(self: &Self) -> &ControlAxis<Dimension3<T>>{
+    pub fn acceleration(self: &Self) -> &T{
         &self.acceleration
     }
-    pub fn acceleration_mut(self: &mut Self) -> &mut ControlAxis<Dimension3<T>>{
+    pub fn acceleration_mut(self: &mut Self) -> &mut T{
         &mut self.acceleration
     }
 
-    pub fn velocity(self: &Self) -> &ControlAxis<Dimension3<T>>{
+    pub fn velocity(self: &Self) -> &T{
         &self.velocity
     }
-    pub fn velocity_mut(self: &mut Self) -> &ControlAxis<Dimension3<T>>{
+    pub fn velocity_mut(self: &mut Self) -> &mut T{
         &mut self.velocity
     }
 }
